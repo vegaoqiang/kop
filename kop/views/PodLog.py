7416f4686402619d61b20f7b0dcfc6af873abbcc
gaox <vegaoqiang@aliyun.com>
2026-01-01 14:52:18 +0800

    add escape key

--- kop/views/PodLog.py
+++ kop/views/PodLog.py
@@ -4,16 +4,24 @@ from kop.components.Log import Logs
 from kop.kube.logs import PodLogs
 from kop.kube.client import KbsAuthLoader
 from kop.models import PodViewModel
+from kop.components.Log import LogController
 
 
 
 class PodLog(Screen):
 
+    BINDINGS = [
+        ("escape", "close", "Close"),
+    ]
+
     def __init__(self, client: KbsAuthLoader, pod: PodViewModel, container_name: str) -> None:
         super().__init__()
-        self.log_handler = PodLogs(client.api_client, pod.name, pod.namespace, container_name)
+        self.pod_logs = PodLogs(client.api_client, pod.name, pod.namespace, container_name)
         
 
     def compose(self) -> ComposeResult:
-        yield Logs(log_handler=self.log_handler)
+        yield Logs(log_controller=LogController(pod_logs=self.pod_logs))
 
+
+    def action_close(self) -> None:
+        self.app.pop_screen()
