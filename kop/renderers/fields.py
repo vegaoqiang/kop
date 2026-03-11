90d8b39df043197162fd6e389ae9873d5a90466d
gaox <vegaoqiang@aliyun.com>
2026-03-11 22:35:43 +0800

    add command for fields.py

--- kop/renderers/fields.py
+++ kop/renderers/fields.py
@@ -1,3 +1,10 @@
+"""
+The functions in this file are used to render fields specified 
+in the Models in the UI. The function names are defined as `resource_field_renderer`, 
+such as the `status` field in the `PodViewModel` corresponding to the 
+`pod_status_renderer` function.
+"""
+
 from rich.text import Text
 
 
