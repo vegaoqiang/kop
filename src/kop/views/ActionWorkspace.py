2eb3075ae7d8d996a8480aa41c38a3b798e467d0
gaox <vegaoqiang@aliyun.com>
2026-05-11 15:05:59 +0800

    binding key(ctrl+←/→) to switch tab

--- src/kop/views/ActionWorkspace.py
+++ src/kop/views/ActionWorkspace.py
@@ -5,7 +5,7 @@ from textual.binding import Binding
 from textual.css.query import NoMatches
 from textual.screen import Screen
 from textual.widgets import TabbedContent, TabPane, Footer, Header
-from textual.widgets._tabbed_content import ContentTab
+from textual.widgets._tabbed_content import ContentTab, ContentTabs
 from textual.widget import Widget
 from kop.widgets.Pty import PodPty
 from uuid import uuid4
@@ -20,8 +20,10 @@ class ActionWorkspace(Screen):
     SUB_TITLE = "Action Workspace"
 
     BINDINGS = [
-        Binding("ctrl+tab", "switch_tab('next')", "Next Tab"),
-        Binding("ctrl+shift+tab", "switch_tab('previous')", "Previous Tab"),
+        Binding("ctrl+right", "switch_tab('next')", "Next Tab", show=True),
+        Binding("ctrl+left", "switch_tab('previous')", "Previous Tab", show=True),
+        Binding("ctrl+f", "switch_tab('next')", "Next Tab", show=False),
+        Binding("ctrl+b", "switch_tab('previous')", "Previous Tab", show=False),
         Binding("ctrl+right_square_bracket", "back_resource", "Back to Resource"),
         Binding("ctrl+w", "close_current_tab", "Close Current Tab"),
     ]
@@ -78,6 +80,18 @@ class ActionWorkspace(Screen):
             return
         await self._close_pane(active_tab_id)
 
+    def action_switch_tab(self, direction: str) -> None:
+        tabbed_content = self.query_one("#tabbed-content", TabbedContent)
+        if tabbed_content.tab_count < 2:
+            return
+
+        tabs = tabbed_content.get_child_by_type(ContentTabs)
+        if direction == "next":
+            tabs.action_next_tab()
+            return
+        if direction == "previous":
+            tabs.action_previous_tab()
+
     @on(events.Click, "ContentTab")
     async def on_content_tab_click(self, event: events.Click) -> None:
         tab = event.widget
@@ -116,4 +130,3 @@ class ActionWorkspace(Screen):
         if not active_tab_id:
             return
         await self._close_pane(active_tab_id)
-
